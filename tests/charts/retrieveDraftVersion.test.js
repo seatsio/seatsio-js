3c074a9e4ca49bfb0e43b99030061f381661af3c
namfal <namifalhan@gmail.com>
2018-10-24 18:35:03 +0200

    Added the test for retrieveDraftVersion

---
+++ tests/charts/retrieveDraftVersion.test.js
@@ -0,0 +1,11 @@
+const testUtils = require('../testUtils.js');
+
+test('should retrieve draft version of a chart', async ()=> {
+  const user = await testUtils.createTestUser();
+  var client = testUtils.createClient(user.secretKey, testUtils.baseUrl);
+  var chart = await client.charts.create();
+  await client.events.create(chart.key);
+  await client.charts.update(chart.key, 'New name');
+  var retrievedChart = await client.charts.retrieveDraftVersion(chart.key);
+  expect(retrievedChart.name).toBe('New name');
+});
