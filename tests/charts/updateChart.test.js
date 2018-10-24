979927db7b5e6cf6e56b05ae452f6076ca82a6fb
namfal <namifalhan@gmail.com>
2018-10-24 18:53:20 +0200

    Added tests for update method for Charts module

---
+++ tests/charts/updateChart.test.js
@@ -0,0 +1,35 @@
+const testUtils = require('../testUtils.js');
+
+test('should update chart name', async ()=> {
+  const user = await testUtils.createTestUser();
+  var client = testUtils.createClient(user.secretKey, testUtils.baseUrl);
+  var categories = [
+    {
+      'key': 1,
+      'label': 'Category 1',
+      'color': '#aaaaaa'
+    }
+  ];
+  var chart = await client.charts.create(null, null, categories);
+  await client.charts.update(chart.key, 'aChart');
+  var retrievedChart = await client.charts.retrievePublishedVersion(chart.key);
+  expect(retrievedChart.name).toBe('aChart');
+  expect(retrievedChart.categories.list).toEqual(categories);
+});
+
+test('should update chart categories', async ()=> {
+  const user = await testUtils.createTestUser();
+  var client = testUtils.createClient(user.secretKey, testUtils.baseUrl);
+  var chart = await client.charts.create('aChart')
+  var categories = [
+    {
+      'key': 1,
+      'label': 'Category 1',
+      'color': '#aaaaaa'
+    }
+  ];
+  await client.charts.update(chart.key, null, categories);
+  var retrievedChart = await client.charts.retrievePublishedVersion(chart.key);
+  expect(retrievedChart.name).toBe('aChart');
+  expect(retrievedChart.categories.list).toEqual(categories);
+});
