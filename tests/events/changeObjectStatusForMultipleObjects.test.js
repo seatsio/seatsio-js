72cefccd9e7a898d2eb4846b838b31096552609c
namfal <namifalhan@gmail.com>
2018-10-26 18:56:43 +0200

    Added tests for changing object status for multiple objects

---
+++ tests/events/changeObjectStatusForMultipleObjects.test.js
@@ -0,0 +1,142 @@
+const testUtils = require('../testUtils.js');
+const ObjectStatus = require('../../src/Events/ObjectStatus.js');
+const ObjectProperties = require('../../src/Events/ObjectProperties.js')
+
+test('should change object status for multiple objects as an array of string', async () => {
+  var user = await testUtils.createTestUser();
+  var client = testUtils.createClient(user.secretKey, testUtils.baseUrl);
+  var chartKey = testUtils.getChartKey();
+  await testUtils.createTestChart(chartKey, user.designerKey);
+  var event = await client.events.create(chartKey);
+  await client.events.changeObjectStatus(event.key, ['A-1', 'A-2'], 'lolzor')
+  var objStatus1 = await client.events.retrieveObjectStatus(event.key, 'A-1')
+  var objStatus2 = await client.events.retrieveObjectStatus(event.key, 'A-2')
+
+  expect(objStatus1.status).toBe('lolzor');
+  expect(objStatus2.status).toBe('lolzor');
+});
+
+test('should change object status for multiple objects as an array of classes', async () => {
+  var user = await testUtils.createTestUser();
+  var client = testUtils.createClient(user.secretKey, testUtils.baseUrl);
+  var chartKey = testUtils.getChartKey();
+  await testUtils.createTestChart(chartKey, user.designerKey);
+  var event = await client.events.create(chartKey);
+  var objects = [new ObjectProperties('A-1'), new ObjectProperties('A-2')];
+  await client.events.changeObjectStatus(event.key, objects,'lolzor');
+  var objStatus1 = await client.events.retrieveObjectStatus(event.key, 'A-1');
+  var objStatus2 = await client.events.retrieveObjectStatus(event.key, 'A-2');
+
+  expect(objStatus1.status).toBe('lolzor');
+  expect(objStatus2.status).toBe('lolzor');
+});
+
+test('should change object status for multiple objects as an array of classes with general admission', async () => {
+  var user = await testUtils.createTestUser();
+  var client = testUtils.createClient(user.secretKey, testUtils.baseUrl);
+  var chartKey = testUtils.getChartKey();
+  await testUtils.createTestChart(chartKey, user.designerKey);
+  var event = await client.events.create(chartKey);
+  var objects = [{objectId : 'A-1'}, {objectId: 'GA1', quantity: 5}];
+
+  await client.events.changeObjectStatus(event.key, objects,'lolzor');
+  var objStatusA1= await client.events.retrieveObjectStatus(event.key, 'A-1');
+  var objStatusGA1 = await client.events.retrieveObjectStatus(event.key, 'GA1');
+
+  expect(objStatusA1.status).toBe('lolzor');
+  expect(objStatusGA1.quantity).toBe(5);
+});
+
+test('should change object status for multiple objects as an array of classes and strings', async () => {
+  var user = await testUtils.createTestUser();
+  var client = testUtils.createClient(user.secretKey, testUtils.baseUrl);
+  var chartKey = testUtils.getChartKey();
+  await testUtils.createTestChart(chartKey, user.designerKey);
+  var event = await client.events.create(chartKey);
+  var objects = ['A-1', {objectId: 'GA1', quantity: 5}];
+
+  await client.events.changeObjectStatus(event.key, objects,'lolzor');
+  var objStatusA1= await client.events.retrieveObjectStatus(event.key, 'A-1');
+  var objStatusGA1 = await client.events.retrieveObjectStatus(event.key, 'GA1');
+
+  expect(objStatusA1.status).toBe('lolzor');
+  expect(objStatusGA1.quantity).toBe(5);
+});
+
+test('should change object status for multiple objects with ticket types', async () => {
+  var user = await testUtils.createTestUser();
+  var client = testUtils.createClient(user.secretKey, testUtils.baseUrl);
+  var chartKey = testUtils.getChartKey();
+  await testUtils.createTestChart(chartKey, user.designerKey);
+  var event = await client.events.create(chartKey);
+  var objects = [
+    (new ObjectProperties('A-1')).setTicketType('T1'),
+    (new ObjectProperties('A-2')).setTicketType('T2')
+    ];
+
+  await client.events.changeObjectStatus(event.key, objects,'lolzor');
+  var status1= await client.events.retrieveObjectStatus(event.key, 'A-1');
+  var status2 = await client.events.retrieveObjectStatus(event.key, 'A-2');
+
+  expect(status1.status).toBe('lolzor');
+  expect(status1.ticketType).toBe('T1');
+  expect(status2.status).toBe('lolzor');
+  expect(status2.ticketType).toBe('T2');
+});
+
+test('should change object status for multiple objects with quantity', async () => {
+  var user = await testUtils.createTestUser();
+  var client = testUtils.createClient(user.secretKey, testUtils.baseUrl);
+  var chartKey = testUtils.getChartKey();
+  await testUtils.createTestChart(chartKey, user.designerKey);
+  var event = await client.events.create(chartKey);
+  var objects = [
+    (new ObjectProperties('GA1')).setQuantity(5),
+    (new ObjectProperties('34')).setQuantity(10)
+    ];
+
+  await client.events.changeObjectStatus(event.key, objects,'lolzor');
+  var status1= await client.events.retrieveObjectStatus(event.key, 'GA1');
+  var status2 = await client.events.retrieveObjectStatus(event.key, '34');
+
+  expect(status1.quantity).toBe(5);
+  expect(status2.quantity).toBe(10);
+});
+
+test('should change object status for multiple objects, GAs and Seats', async () => {
+  var user = await testUtils.createTestUser();
+  var client = testUtils.createClient(user.secretKey, testUtils.baseUrl);
+  var chartKey = testUtils.getChartKey();
+  await testUtils.createTestChart(chartKey, user.designerKey);
+  var event = await client.events.create(chartKey);
+  var objects = [
+    (new ObjectProperties('GA1')).setQuantity(5),
+    new ObjectProperties('A-1')
+    ];
+
+  await client.events.changeObjectStatus(event.key, objects,'lolzor');
+  var status1= await client.events.retrieveObjectStatus(event.key, 'GA1');
+  var status2 = await client.events.retrieveObjectStatus(event.key, 'A-1');
+
+  expect(status1.quantity).toBe(5);
+  expect(status2.status).toBe('lolzor');
+});
+
+test('should change object status for multiple objects with extra data', async () => {
+  var user = await testUtils.createTestUser();
+  var client = testUtils.createClient(user.secretKey, testUtils.baseUrl);
+  var chartKey = testUtils.getChartKey();
+  await testUtils.createTestChart(chartKey, user.designerKey);
+  var event = await client.events.create(chartKey);
+  var objects = [
+    (new ObjectProperties('A-1')).setExtraData({"foo": "bar"}),
+    (new ObjectProperties('A-2')).setExtraData({"foo": "baz"})
+    ];
+
+  await client.events.changeObjectStatus(event.key, objects,'lolzor');
+  var status1= await client.events.retrieveObjectStatus(event.key, 'A-1');
+  var status2 = await client.events.retrieveObjectStatus(event.key, 'A-2');
+
+  expect(status1.extraData).toEqual({"foo" : "bar"});
+  expect(status2.extraData).toEqual({"foo" : "baz"});
+});
