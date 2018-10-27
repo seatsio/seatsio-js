ff282adad8e6aea0904e9d6c3bb343c78678d54b
namfal <namifalhan@gmail.com>
2018-10-27 16:12:33 +0200

    Added retrieve method for hold tokens and its test

--- src/HoldTokens/HoldTokens.js
+++ src/HoldTokens/HoldTokens.js
@@ -10,6 +10,16 @@ class HoldTokens{
     }
     return this.client.post('/hold-tokens', request).then( (res) => res.data);
   }
+
+  retrieve(holdToken){
+    return this.client.get(`/hold-tokens/${holdToken}`).then( (res) => res.data);
+  }
+
+  expiresInMinutes(holdToken, minutes){
+    var request = {};
+    request.expiresInMinutes = minutes;
+    return this.client.post(`/hold-tokens/${holdToken}`, request).then( (res) => res.data);
+  }
 }
 
 module.exports = HoldTokens;
---
+++ tests/holdTokens/retrieveHoldToken.test.js
@@ -0,0 +1,12 @@
+const testUtils = require('../testUtils.js');
+
+test('should retrieve hold tokens', async ()=> {
+  var user = await testUtils.createTestUser();
+  var client = testUtils.createClient(user.secretKey, testUtils.baseUrl);
+  var holdToken = await client.holdTokens.create();
+  var retrievedToken = await client.holdTokens.retrieve(holdToken.holdToken);
+  expect(retrievedToken.holdToken).toBe(holdToken.holdToken);
+  expect(retrievedToken.expiresAt).toBe(holdToken.expiresAt);
+  expect(holdToken.expiresInSeconds).toBeGreaterThanOrEqual(14*60);
+  expect(holdToken.expiresInSeconds).toBeLessThanOrEqual(15*60);
+});
