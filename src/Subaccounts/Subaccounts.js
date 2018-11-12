1711c943f1f06cbe4654e6e70b12600cb23b9a26
namfal <10213761+namfal@users.noreply.github.com>
2018-11-12 11:46:53 +0100

    Reformatted

--- src/Subaccounts/Subaccounts.js
+++ src/Subaccounts/Subaccounts.js
@@ -12,23 +12,22 @@ class Subaccounts {
         this.inactive = new AsyncIterator('/subaccounts/inactive', this.client, 'subaccounts');
     }
 
-    /**
-     * @return Subaccount
-     */
+    /* @return Subaccount */
+    retrieve(id) {
+        return this.client.get(`/subaccounts/${id}`).then((res) => utilities.createSubaccount(res.data));
+    }
+
+    /* @return Subaccount */
     create(name = null) {
         return this.doCreate(null, name);
     }
 
-    /**
-     * @return Subaccount
-     */
+    /* @return Subaccount */
     createWithEmail(email, name = null) {
         return this.doCreate(email, name);
     }
 
-    /**
-     * @then Subaccount
-     */
+    /* @then Subaccount */
     doCreate(email = null, name = null) {
         let requestParameters = {};
 
@@ -42,40 +41,10 @@ class Subaccounts {
 
 
         return this.client.post('/subaccounts', requestParameters)
-                          .then((res) => utilities.createSubaccount(res.data));
-    }
-    /**
-     * @return Chart
-     */
-    copyChartToSubaccount(fromId, toId, chartKey) {
-        return this.client.post(`/subaccounts/${fromId}/charts/${chartKey}/actions/copy-to/${toId}`)
-                          .then((res) => utilities.createChart(res.data));
+            .then((res) => utilities.createSubaccount(res.data));
     }
 
-    /**
-     * @return Chart
-     */
-    copyChartToParent(id, chartKey) {
-        return this.client.post(`/subaccounts/${id}/charts/${chartKey}/actions/copy-to/parent`)
-                          .then((res) => utilities.createChart(res.data));
-    }
-
-    activate(id) {
-        return this.client.post(`/subaccounts/${id}/actions/activate`);
-    }
-
-    deactivate(id) {
-        return this.client.post(`/subaccounts/${id}/actions/deactivate`);
-    }
-
-    /**
-     * @return Subaccount
-     */
-    retrieve(id) {
-        return this.client.get(`/subaccounts/${id}`).then((res) => utilities.createSubaccount(res.data));
-    }
-
-    update(id, name, email) {
+    update(id, name = null, email = null) {
         let requestParameters = {};
 
         if (name !== null) {
@@ -89,30 +58,57 @@ class Subaccounts {
         return this.client.post(`/subaccounts/${id}`, requestParameters);
     }
 
+    activate(id) {
+        return this.client.post(`/subaccounts/${id}/actions/activate`);
+    }
+
+    deactivate(id) {
+        return this.client.post(`/subaccounts/${id}/actions/deactivate`);
+    }
+
+    /* @return String */
     regenerateSecretKey(id) {
         return this.client.post(`/subaccounts/${id}/secret-key/actions/regenerate`).then((res) => res.data);
     }
 
+    /* @return String */
     regenerateDesignerKey(id) {
         return this.client.post(`/subaccounts/${id}/designer-key/actions/regenerate`).then((res) => res.data);
     }
 
-    listAll(){
+    /* @return Chart */
+    copyChartToParent(id, chartKey) {
+        return this.client.post(`/subaccounts/${id}/charts/${chartKey}/actions/copy-to/parent`)
+            .then((res) => utilities.createChart(res.data));
+    }
+
+    /* @return Chart */
+    copyChartToSubaccount(fromId, toId, chartKey) {
+        return this.client.post(`/subaccounts/${fromId}/charts/${chartKey}/actions/copy-to/${toId}`)
+            .then((res) => utilities.createChart(res.data));
+    }
+
+    /* @return AsyncIterator */
+    listAll() {
         return new AsyncIterator('/subaccounts', this.client, 'subaccounts');
     }
 
-    listFirstPage(pageSize = null){
+    /* @return Page */
+    listFirstPage(pageSize = null) {
         return this.iterator().firstPage(pageSize);
     }
 
-    listPageAfter(afterId, pageSize = null){
+    /* @return Page */
+    listPageAfter(afterId, pageSize = null) {
         return this.iterator().pageAfter(afterId, null, pageSize);
     }
 
-    listPageBefore(beforeId, pageSize = null){
+    /* @return Page */
+    listPageBefore(beforeId, pageSize = null) {
         return this.iterator().pageBefore(beforeId, null, pageSize);
     }
 
+    /* @return Lister */
     iterator() {
         return new Lister(new PageFetcher('/subaccounts', this.client, results => {
             let subaccounts = results.items.map((subaccountsData) => utilities.createSubaccount(subaccountsData));
