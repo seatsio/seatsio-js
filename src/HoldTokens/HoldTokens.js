d8b517a9d2bf440bc5024261ebcc690bcfcce684
namfal <10213761+namfal@users.noreply.github.com>
2018-11-12 12:30:36 +0100

    Reformatted

--- src/HoldTokens/HoldTokens.js
+++ src/HoldTokens/HoldTokens.js
@@ -5,6 +5,7 @@ class HoldTokens {
         this.client = client;
     }
 
+    /* @return HoldToken */
     create(expiresInMinutes = null) {
         let request = {};
         if (expiresInMinutes !== null) {
@@ -13,15 +14,19 @@ class HoldTokens {
         return this.client.post('/hold-tokens', request).then((res) => utilities.createHoldToken(res.data));
     }
 
-    retrieve(holdToken) {
-        return this.client.get(`/hold-tokens/${holdToken}`).then((res) => utilities.createHoldToken(res.data));
-    }
-
+    /* @return HoldToken */
     expiresInMinutes(holdToken, minutes) {
         let request = {};
         request.expiresInMinutes = minutes;
         return this.client.post(`/hold-tokens/${holdToken}`, request).then((res) => utilities.createHoldToken(res.data));
     }
+
+    /* @return HoldToken */
+    retrieve(holdToken) {
+        return this.client.get(`/hold-tokens/${holdToken}`).then((res) => utilities.createHoldToken(res.data));
+    }
+
+
 }
 
 module.exports = HoldTokens;
