1456b85d2a23780a62527a87e67a67d7f0cd55d8
namfal <10213761+namfal@users.noreply.github.com>
2018-11-13 20:39:37 +0100

    increased setTimeout for tests

--- tests/setupTests.js
+++ tests/setupTests.js
@@ -1,7 +1,7 @@
 const testUtils = require('./testUtils.js');
 
 beforeEach(async () => {
-    jest.setTimeout(60000);
+    jest.setTimeout(80000);
     global.user = await testUtils.createTestUser();
     global.client = testUtils.createClient(user.secretKey, testUtils.baseUrl);
 });
