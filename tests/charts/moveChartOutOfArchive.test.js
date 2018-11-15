88be0caa5c8e8da0b9c1d8a3b2b365cf8c987774
namfal <10213761+namfal@users.noreply.github.com>
2018-11-15 10:56:47 +0100

    small reformatting

--- tests/charts/moveChartOutOfArchive.test.js
+++ tests/charts/moveChartOutOfArchive.test.js
@@ -1,12 +1,12 @@
 test('should move chart out of archive', async () => {
     let chart = await client.charts.create();
     await client.charts.moveToArchive(chart.key);
-    await client.charts.moveOutOfArchive(chart.key);
     let retrievedChartKeys = [];
 
+    await client.charts.moveOutOfArchive(chart.key);
+    
     for await(let chart of client.charts.archive) {
         retrievedChartKeys.push(chart.key)
     }
-
     expect(retrievedChartKeys).toEqual([]);
 });
