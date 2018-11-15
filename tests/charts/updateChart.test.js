ef4947bad0f475fd1ee13f08e5592f9ba972fac0
namfal <10213761+namfal@users.noreply.github.com>
2018-11-15 10:40:40 +0100

    minor formatting

--- tests/charts/updateChart.test.js
+++ tests/charts/updateChart.test.js
@@ -10,7 +10,7 @@ test('should update chart name', async () => {
 });
 
 test('should update chart categories', async () => {
-    let chart = await client.charts.create('aChart')
+    let chart = await client.charts.create('aChart');
     let categories = [{'key': 1, 'label': 'Category 1', 'color': '#aaaaaa'}];
 
     await client.charts.update(chart.key, null, categories);
