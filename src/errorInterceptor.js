1cd015df2acab7c320d2a458efd095dca56cca79
namfal <10213761+namfal@users.noreply.github.com>
2018-11-23 14:41:00 +0100

    Check if error.response.data is not undefined

--- src/errorInterceptor.js
+++ src/errorInterceptor.js
@@ -1,6 +1,6 @@
 function errorResponseHandler(error) {
     return new Promise(function(resolve, reject){
-        if(error.response.data ){
+        if(typeof error.response.data !== 'undefined' && error.response.data){
             reject(error.response.data);
         } else {
             reject(`${error.config.method} ${error.config.url} resulted in ${error.response.status}  ${error.response.statusText}  error`)
