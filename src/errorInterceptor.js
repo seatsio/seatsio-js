ac214af41541a800ab94fbe0fe7b08b1a930ed7f
namfal <10213761+namfal@users.noreply.github.com>
2018-11-23 22:00:54 +0100

    Tweaked error handling

--- src/errorInterceptor.js
+++ src/errorInterceptor.js
@@ -1,9 +1,11 @@
 function errorResponseHandler(error) {
     return new Promise(function(resolve, reject){
-        if(typeof error.response.data !== 'undefined' && error.response.data){
-            reject(error.response.data);
-        } else if(typeof error.response.statusText !== 'undefined' && error.response.statusText){
-            reject(`${error.config.method} ${error.config.url} resulted in ${error.response.status}  ${error.response.statusText}  error`)
+        if(typeof error.response !== 'undefined'){
+            if(typeof error.response.data !== 'undefined' && error.response.data){
+                reject(error.response.data);
+            } else if(typeof error.response.statusText !== 'undefined' && error.response.statusText){
+                reject(`${error.config.method} ${error.config.url} resulted in ${error.response.status}  ${error.response.statusText}  error`)
+            }
         } else {
             console.log(error);
             reject(error);
