09d04ec636064651611efbf6eea23e2c3b9e9c8d
Nami Falhan <10213761+namfal@users.noreply.github.com>
2019-02-18 08:27:40 +0100

    Exporting some additrional helper classes for extrenal use

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-module.exports = require('./src/SeatsioClient.js');
+module.exports = {
+  SeatsioClient: require('./src/SeatsioClient.js'),
+  StatusChangesParams: require('./src/Events/StatusChangesParams.js'),
+  ChartListParams: require('./src/Charts/ChartListParams.js'),
+  SubaccountListParams: require('./src/Subaccounts/SubaccountListParams.js')
+}
