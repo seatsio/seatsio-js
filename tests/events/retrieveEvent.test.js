06907aa413b8ca978ce8df60196c65ee9bdc1714
Nami Falhan <10213761+namfal@users.noreply.github.com>
2019-02-18 07:46:25 +0100

    Fixed a failing test

--- tests/events/retrieveEvent.test.js
+++ tests/events/retrieveEvent.test.js
@@ -3,8 +3,8 @@ const testUtils = require('../testUtils.js')
 test('should retrieve event', async () => {
   let chartKey = testUtils.getChartKey()
   await testUtils.createTestChart(chartKey, user.designerKey)
-  let event = await client.events.create(chartKey)
   let now = new Date()
+  let event = await client.events.create(chartKey)
 
   let retrievedEvent = await client.events.retrieve(event.key)
 
@@ -14,8 +14,8 @@ test('should retrieve event', async () => {
   expect(retrievedEvent.bookWholeTables).toBe(false)
   expect(retrievedEvent.supportsBestAvailable).toBe(true)
   expect(retrievedEvent.createdOn).toEqual(event.createdOn)
-  expect(retrievedEvent.createdOn.getTime()).toBeLessThanOrEqual(now.getTime())
-  expect(retrievedEvent.createdOn.getTime()).toBeGreaterThanOrEqual((now.getTime() - 1000))
+  expect(retrievedEvent.createdOn.getTime()).toBeLessThanOrEqual((now.getTime() + 1000))
+  expect(retrievedEvent.createdOn.getTime()).toBeGreaterThanOrEqual(now.getTime())
   expect(retrievedEvent.forSaleConfig).toBeFalsy()
   expect(retrievedEvent.updatedOn).toBeNull()
 })
