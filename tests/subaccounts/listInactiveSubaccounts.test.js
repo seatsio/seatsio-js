d43982b4780e4b69d34c3b110a502ca5f4b1c12a
Nami Falhan <10213761+namfal@users.noreply.github.com>
2019-03-15 11:18:49 +0100

    added some tests for active subaccounts paginated methods

--- tests/subaccounts/listInactiveSubaccounts.test.js
+++ tests/subaccounts/listInactiveSubaccounts.test.js
@@ -12,3 +12,18 @@ test('should list all inactive subaccounts', async () => {
 
   expect(inactiveSubaccountIds.sort()).toEqual([subaccount1.id, subaccount2.id].sort())
 })
+
+test('should list first page of inactive subaccounts', async () => {
+  let subaccount1 = await client.subaccounts.create()
+  let subaccount2 = await client.subaccounts.create()
+  let subaccount3 = await client.subaccounts.create()
+  await client.subaccounts.create()
+  await client.subaccounts.deactivate(subaccount1.id)
+  await client.subaccounts.deactivate(subaccount2.id)
+  await client.subaccounts.deactivate(subaccount3.id)
+
+  let firstPage = await client.subaccounts.inactive.firstPage()
+
+  expect(firstPage.items.map(item => item.id).sort()).toEqual([subaccount1.id, subaccount2.id, subaccount3.id].sort())
+  expect(firstPage.items.length).toBe(3)
+})
