c9b99532d593a6b66df1fda655ce618f6708657c
Nami Falhan <10213761+namfal@users.noreply.github.com>
2019-03-18 08:40:16 +0100

    applied Preserve Whole Object to AccountSettings

--- src/Accounts/Account.js
+++ src/Accounts/Account.js
@@ -1,4 +1,3 @@
-const ChartValidationSettings = require('./ChartValidationSettings.js')
 const AccountSettings = require('./AccountSettings.js')
 
 class Account {
@@ -6,12 +5,10 @@ class Account {
    * @param {object} account
    */
   constructor (account) {
-    let chartValidationSettings = new ChartValidationSettings(account.settings.chartValidation)
-
     this.secretKey = account.secretKey
     this.designerKey = account.designerKey
     this.publicKey = account.publicKey
-    this.settings = new AccountSettings(account.settings.draftChartDrawingsEnabled, account.settings.holdOnSelectForGAs, account.settings.holdPeriodInMinutes, chartValidationSettings)
+    this.settings = new AccountSettings(account.settings)
     this.email = account.email
     this.role = account.role
   }
