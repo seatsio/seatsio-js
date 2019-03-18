758c51e56a755df076a73bb4bb0c544bebb5db05
Nami Falhan <10213761+namfal@users.noreply.github.com>
2019-03-18 08:36:18 +0100

    applied preserve whole object to ChartValidationSettings

--- src/Accounts/Account.js
+++ src/Accounts/Account.js
@@ -6,8 +6,7 @@ class Account {
    * @param {object} account
    */
   constructor (account) {
-    let chartValidation = account.settings.chartValidation
-    let chartValidationSettings = new ChartValidationSettings(chartValidation.VALIDATE_DUPLICATE_LABELS, chartValidation.VALIDATE_OBJECTS_WITHOUT_CATEGORIES, chartValidation.VALIDATE_UNLABELED_OBJECTS, chartValidation.VALIDATE_FOCAL_POINT, chartValidation.VALIDATE_OBJECT_TYPES_PER_CATEGORY, account.settings.chartValidation)
+    let chartValidationSettings = new ChartValidationSettings(account.settings.chartValidation)
 
     this.secretKey = account.secretKey
     this.designerKey = account.designerKey
