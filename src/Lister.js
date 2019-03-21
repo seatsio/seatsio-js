0dfe8d3550cc07eddb92c8aaaac6ee4fa8fa5417
Nami Falhan <10213761+namfal@users.noreply.github.com>
2019-03-21 10:36:35 +0100

    added parameter documentation for lister

--- src/Lister.js
+++ src/Lister.js
@@ -9,21 +9,42 @@ class Lister {
     this.type = type
   }
 
+  /**
+   * @param {object} parameters
+   * @returns {AsyncIterator}
+   */
   all (parameters = {}) {
     let params = parameters.serialize ? parameters.serialize() : parameters
     return new AsyncIterator(this.url, this.client, this.type, params)
   }
 
+  /**
+   * @param {?object} queryParams
+   * @param {?number} pageSize
+   * @returns {Page}
+   */
   firstPage (queryParams = null, pageSize = null) {
     let params = queryParams ? queryParams.serialize() : null
     return this.pageFetcher.fetchAfter(null, params, pageSize)
   }
 
+  /**
+   * @param {string} afterId
+   * @param {?object} queryParams
+   * @param {?number} pageSize
+   * @returns {Page}
+   */
   pageAfter (afterId, queryParams = null, pageSize = null) {
     let params = queryParams ? queryParams.serialize() : null
     return this.pageFetcher.fetchAfter(afterId, params, pageSize)
   }
 
+  /**
+   * @param {string} beforeId
+   * @param {?object} queryParams
+   * @param {?number} pageSize
+   * @returns {Page}
+   */
   pageBefore (beforeId, queryParams = null, pageSize = null) {
     let params = queryParams ? queryParams.serialize() : null
     return this.pageFetcher.fetchBefore(beforeId, params, pageSize)
