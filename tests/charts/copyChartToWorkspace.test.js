688e9d7fa4aece4bdb83db9367735702bf0fc83f
Victor Nahuel Chaves <nahuel.chaves@gmail.com>
2020-01-27 15:01:10 -0300

    add copyToWorkspace method to Charts.js (#45)
    add copyToWorkspace test

---
+++ tests/charts/copyChartToWorkspace.test.js
@@ -0,0 +1,14 @@
+const testUtils = require('../testUtils.js')
+
+test('should copy to workspace', async () => {
+    const workspace = await client.workspaces.create('a workspace')
+    const workspaceClient = testUtils.createClient(user.secretKey, workspace.key)
+    const chart = await client.charts.create('My chart')
+
+    const copiedChart = await client.charts.copyToWorkspace(chart.key, workspace.key)
+    const retrievedChart = await workspaceClient.charts.retrieve(copiedChart.key)
+
+    expect(copiedChart.name).toEqual('My chart')
+    expect(copiedChart.key).not.toBe(chart.key)
+    expect(retrievedChart.name).toEqual('My chart')
+})
