c8107e5d5f4e3124660a97f4a2ff83849d629cef
Victor Nahuel Chaves <nahuel.chaves@gmail.com>
2020-01-27 14:03:26 -0300

    fix tests in master (#46)

--- tests/workspaces/listPageAfter.test.js
+++ tests/workspaces/listPageAfter.test.js
@@ -1,7 +1,7 @@
 test('should list workspaces after an id', async () => {
-    const ws1 = await client.workspaces.create('')
-    const ws2 = await client.workspaces.create('')
-    const ws3 = await client.workspaces.create('')
+    const ws1 = await client.workspaces.create('w1')
+    const ws2 = await client.workspaces.create('w2')
+    const ws3 = await client.workspaces.create('w3')
 
     const page = await client.workspaces.listPageAfter(ws3.id, null, 2)
 
@@ -9,12 +9,12 @@ test('should list workspaces after an id', async () => {
 })
 
 test('should filter workspaces after an id', async () => {
-    const ws1 = await client.workspaces.create('foo')
+    const ws1 = await client.workspaces.create('workspace1')
     await client.workspaces.create('bar')
-    const ws3 = await client.workspaces.create('foo')
-    const ws4 = await client.workspaces.create('foo')
+    const ws3 = await client.workspaces.create('workspace2')
+    const ws4 = await client.workspaces.create('workspace3')
 
-    const page = await client.workspaces.listPageAfter(ws4.id, 'fo', 2)
+    const page = await client.workspaces.listPageAfter(ws4.id, 'wo', 2)
 
     expect(page.items.map(workspace => workspace.id)).toEqual([ws3.id, ws1.id])
 })
