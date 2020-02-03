899a7a8fcabbe3526a9e9f0250d733cf63b920b7
Nami Falhan <10213761+namfal@users.noreply.github.com>
2020-02-03 12:46:16 +0100

    removed irrelevant tests

--- tests/subaccounts/createSubAccount.test.js
+++ tests/subaccounts/createSubAccount.test.js
@@ -1,6 +1,4 @@
-const testUtils = require('../testUtils.js')
-
-test('should create subaccount', async () => {
+test('should create subaccount with name', async () => {
     let subaccount = await client.subaccounts.create('subaccountTest')
 
     expect(subaccount.secretKey).toBeTruthy()
@@ -8,6 +6,7 @@ test('should create subaccount', async () => {
     expect(subaccount.publicKey).toBeTruthy()
     expect(subaccount.name).toBe('subaccountTest')
     expect(subaccount.active).toBe(true)
+    expect(subaccount.email).toBeFalsy()
 })
 
 test('name is generated in subaccount create', async () => {
@@ -15,29 +14,4 @@ test('name is generated in subaccount create', async () => {
 
     expect(subaccount.name).toBeTruthy()
     expect(subaccount.email).toBeFalsy()
-})
-
-test('subaccount create with email parameter does nothing', async () => {
-    let randomEmail = testUtils.getRandomEmail()
-
-    let subaccount = await client.subaccounts.createWithEmail(randomEmail, null)
-
-    expect(subaccount.secretKey).toBeTruthy()
-    expect(subaccount.designerKey).toBeTruthy()
-    expect(subaccount.publicKey).toBeTruthy()
-    expect(subaccount.email).toBeFalsy()
-    expect(subaccount.name).toBeTruthy()
-})
-
-test('subaccount create with name and email', async () => {
-    let randomEmail = testUtils.getRandomEmail()
-
-    let subaccount = await client.subaccounts.createWithEmail(randomEmail, 'jeff')
-
-    expect(subaccount.secretKey).toBeTruthy()
-    expect(subaccount.designerKey).toBeTruthy()
-    expect(subaccount.publicKey).toBeTruthy()
-    expect(subaccount.email).toBeFalsy()
-    expect(subaccount.name).toBe('jeff')
-    expect(subaccount.active).toBe(true)
 })
