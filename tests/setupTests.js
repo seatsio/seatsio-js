9981401d1ab9d5d227d60385de5c5b8192038bcb
Nami Falhan <10213761+namfal@users.noreply.github.com>
2020-02-04 20:46:42 +0100

    inscreased timeout to 35000

--- tests/setupTests.js
+++ tests/setupTests.js
@@ -1,7 +1,7 @@
 const testUtils = require('./testUtils.js')
 
 beforeEach(async () => {
-    jest.setTimeout(30000)
+    jest.setTimeout(35000)
     global.user = await testUtils.createTestUser()
     global.client = testUtils.createClient(user.secretKey)
 })
