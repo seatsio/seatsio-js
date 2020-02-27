12c08b7b4bafa074cae45b2a92acc0a964441e6e
Nahuel Chaves <nahuel.chaves@gmail.com>
2020-02-27 15:10:59 -0300

    add invitation id assertion

--- tests/invitations/listAllInvitations.test.js
+++ tests/invitations/listAllInvitations.test.js
@@ -1,7 +1,7 @@
 const testUtils = require('../testUtils')
 
 test('invite users', async () => {
-    const { client, user } = await testUtils.createTestUserAndClient()
+    const { client } = await testUtils.createTestUserAndClient()
     const email1 = testUtils.getRandomEmail()
     await client.users.invite(email1, 'COMPANY_ADMIN')
     const email2 = testUtils.getRandomEmail()
@@ -10,8 +10,10 @@ test('invite users', async () => {
     const invitations = await client.invitations.listAll()
 
     expect(invitations.length).toBe(2)
+    expect(invitations[0].id).toBeDefined()
     expect(invitations[0].email).toBe(email2)
     expect(invitations[0].date).toBeInstanceOf(Date)
+    expect(invitations[1].id).toBeDefined()
     expect(invitations[1].email).toBe(email1)
     expect(invitations[1].date).toBeInstanceOf(Date)
 })
