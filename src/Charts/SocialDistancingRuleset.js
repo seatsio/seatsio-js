75ffffe77ef3a23988488b1efb2a5461b4244d90
mroloux <matti@seats.io>
2020-10-08 13:00:44 +0200

    Added fixedGroupLayout, maxOccupancyAbsolute nad maxOccupancyPercentage to SocialDistancingRuleset

--- src/Charts/SocialDistancingRuleset.js
+++ src/Charts/SocialDistancingRuleset.js
@@ -1,13 +1,24 @@
 class SocialDistancingRuleset {
-    constructor (index, name, numberOfDisabledSeatsToTheSides, disableSeatsInFrontAndBehind, numberOfDisabledAisleSeats, maxGroupSize, disabledSeats, enabledSeats) {
+    constructor (index, name, numberOfDisabledSeatsToTheSides, disableSeatsInFrontAndBehind, numberOfDisabledAisleSeats, maxGroupSize, maxOccupancyAbsolute, maxOccupancyPercentage, fixedGroupLayout, disabledSeats, enabledSeats) {
         this.index = index
         this.name = name
         this.numberOfDisabledSeatsToTheSides = numberOfDisabledSeatsToTheSides
         this.disableSeatsInFrontAndBehind = disableSeatsInFrontAndBehind
         this.numberOfDisabledAisleSeats = numberOfDisabledAisleSeats
         this.maxGroupSize = maxGroupSize
+        this.maxOccupancyAbsolute = maxOccupancyAbsolute
+        this.maxOccupancyPercentage = maxOccupancyPercentage
+        this.fixedGroupLayout = fixedGroupLayout
         this.disabledSeats = disabledSeats
         this.enabledSeats = enabledSeats
+    }
+
+    static fixed (index, name, disabledSeats) {
+        return new SocialDistancingRuleset(index, name, undefined, undefined, undefined, undefined, undefined, undefined, true, disabledSeats, undefined)
+    }
+
+    static ruleBased (index, name, numberOfDisabledSeatsToTheSides, disableSeatsInFrontAndBehind, numberOfDisabledAisleSeats, maxGroupSize, maxOccupancyAbsolute, maxOccupancyPercentage, disabledSeats, enabledSeats) {
+        return new SocialDistancingRuleset(index, name, numberOfDisabledSeatsToTheSides, disableSeatsInFrontAndBehind, numberOfDisabledAisleSeats, maxGroupSize, maxOccupancyAbsolute, maxOccupancyPercentage, false, disabledSeats, enabledSeats)
     }
 
     static fromJson (json) {
@@ -18,6 +29,9 @@ class SocialDistancingRuleset {
             json.disableSeatsInFrontAndBehind,
             json.numberOfDisabledAisleSeats,
             json.maxGroupSize,
+            json.maxOccupancyAbsolute,
+            json.maxOccupancyPercentage,
+            json.fixedGroupLayout,
             json.disabledSeats,
             json.enabledSeats
         )
--- tests/charts/saveSocialDistancingRulesets.test.js
+++ tests/charts/saveSocialDistancingRulesets.test.js
@@ -5,7 +5,7 @@ test('should save rulesets', async () => {
     const { client } = await testUtils.createTestUserAndClient()
     const chart = await client.charts.create()
     const rulesets = {
-        ruleset1: new SocialDistancingRuleset(0, 'My first ruleset', 1, true, 2, 1, ['A-1'], ['A-2']),
+        ruleset1: new SocialDistancingRuleset(0, 'My first ruleset', 1, true, 2, 1, 10, 0, false, ['A-1'], ['A-2']),
         ruleset2: new SocialDistancingRuleset(1, 'My second ruleset')
     }
 
@@ -21,7 +21,10 @@ test('should save rulesets', async () => {
             enabledSeats: ['A-2'],
             maxGroupSize: 1,
             numberOfDisabledAisleSeats: 2,
-            numberOfDisabledSeatsToTheSides: 1
+            numberOfDisabledSeatsToTheSides: 1,
+            maxOccupancyAbsolute: 10,
+            maxOccupancyPercentage: 0,
+            fixedGroupLayout: false
         },
         ruleset2: {
             index: 1,
@@ -31,7 +34,64 @@ test('should save rulesets', async () => {
             enabledSeats: [],
             maxGroupSize: 0,
             numberOfDisabledAisleSeats: 0,
-            numberOfDisabledSeatsToTheSides: 0
+            numberOfDisabledSeatsToTheSides: 0,
+            maxOccupancyAbsolute: 0,
+            maxOccupancyPercentage: 0,
+            fixedGroupLayout: false
+        }
+    })
+})
+
+test('should save fixed ruleset', async () => {
+    const { client } = await testUtils.createTestUserAndClient()
+    const chart = await client.charts.create()
+    const rulesets = {
+        ruleset1: SocialDistancingRuleset.fixed(0, 'My first ruleset', ['A-1'])
+    }
+
+    await client.charts.saveSocialDistancingRulesets(chart.key, rulesets)
+
+    const retrievedChart = await client.charts.retrieve(chart.key)
+    expect(retrievedChart.socialDistancingRulesets).toEqual({
+        ruleset1: {
+            index: 0,
+            name: 'My first ruleset',
+            disableSeatsInFrontAndBehind: false,
+            disabledSeats: ['A-1'],
+            enabledSeats: [],
+            maxGroupSize: 0,
+            numberOfDisabledAisleSeats: 0,
+            numberOfDisabledSeatsToTheSides: 0,
+            maxOccupancyAbsolute: 0,
+            maxOccupancyPercentage: 0,
+            fixedGroupLayout: true
+        }
+    })
+})
+
+test('should save rule based ruleset', async () => {
+    const { client } = await testUtils.createTestUserAndClient()
+    const chart = await client.charts.create()
+    const rulesets = {
+        ruleset1: new SocialDistancingRuleset(0, 'My first ruleset', 1, true, 2, 1, 10, 0, false, ['A-1'], ['A-2'])
+    }
+
+    await client.charts.saveSocialDistancingRulesets(chart.key, rulesets)
+
+    const retrievedChart = await client.charts.retrieve(chart.key)
+    expect(retrievedChart.socialDistancingRulesets).toEqual({
+        ruleset1: {
+            index: 0,
+            name: 'My first ruleset',
+            disableSeatsInFrontAndBehind: true,
+            disabledSeats: ['A-1'],
+            enabledSeats: ['A-2'],
+            maxGroupSize: 1,
+            numberOfDisabledAisleSeats: 2,
+            numberOfDisabledSeatsToTheSides: 1,
+            maxOccupancyAbsolute: 10,
+            maxOccupancyPercentage: 0,
+            fixedGroupLayout: false
         }
     })
 })
