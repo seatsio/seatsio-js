722dbe1841cae4789a50d273e968e00a543ab5d7
mroloux <matti.roloux@gmail.com>
2021-04-07 16:12:43 +0200

    Exponential backoff when server-side rate limit gets hit (#67)
    * Implemented exponential backoff
    
    * Updated README.md
    
    * Made test stricter
    
    * Fixed tests
    
    Co-authored-by: mroloux <matti@seats.io>

---
+++ tests/exponentialBackoff.test.js
@@ -0,0 +1,37 @@
+const Region = require('../src/Region')
+const { SeatsioClient } = require('../index.js')
+
+test('aborts eventually if server keeps returning 429', async () => {
+    const client = new SeatsioClient(new Region('https://httpbin.org'))
+    const start = new Date()
+    try {
+        await client.client.get('/status/429')
+        throw new Error('Should have failed')
+    } catch (e) {
+        expect(e).toBe('get /status/429 resulted in 429  TOO MANY REQUESTS  error')
+        const waitTime = new Date().getTime() - start.getTime()
+        expect(waitTime).toBeGreaterThan(10000)
+        expect(waitTime).toBeLessThan(20000)
+    }
+})
+
+test('aborts directly if server returns error other than 429', async () => {
+    const client = new SeatsioClient(new Region('https://httpbin.org'))
+    const start = new Date()
+    try {
+        await client.client.get('/status/400')
+        throw new Error('Should have failed')
+    } catch (e) {
+        expect(e).toBe('get /status/400 resulted in 400  BAD REQUEST  error')
+        const waitTime = new Date().getTime() - start.getTime()
+        expect(waitTime).toBeLessThan(2000)
+    }
+})
+
+test('returns successfully when the server sends a 429 first, but then a successful response', async () => {
+    const client = new SeatsioClient(new Region('https://httpbin.org'))
+    for (let i = 0; i < 20; ++i) {
+        const response = await client.client.get('/status/429:0.25,204:0.75')
+        expect(response.status).toBe(204)
+    }
+})
