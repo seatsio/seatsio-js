4d994869742436abb40f30bf961b4f088aa341a6
Matti Roloux <matti@seats.io>
2022-01-27 16:27:14 +0100

    Replaced mockbin by httpbin since mockbin is down

--- tests/exponentialBackoff.test.js
+++ tests/exponentialBackoff.test.js
@@ -2,13 +2,13 @@ const Region = require('../src/Region')
 const { SeatsioClient } = require('../index.js')
 
 test('aborts eventually if server keeps returning 429', async () => {
-    const client = new SeatsioClient(new Region('https://mockbin.org'))
+    const client = new SeatsioClient(new Region('https://httpbin.org'))
     const start = new Date()
     try {
-        await client.client.get('/bin/0381d6f4-0155-4b8c-937b-73d3d88b2a3f')
+        await client.client.get('/status/429')
         throw new Error('Should have failed')
     } catch (e) {
-        expect(e).toEqual({ errors: [{ code: 'RATE_LIMIT_EXCEEDED', message: 'Rate limit exceeded' }], messages: [], requestId: '123456', status: 429 })
+        expect(e).toBe('get /status/429 resulted in 429 TOO MANY REQUESTS error')
         const waitTime = new Date().getTime() - start.getTime()
         expect(waitTime).toBeGreaterThan(10000)
         expect(waitTime).toBeLessThan(20000)
@@ -16,26 +16,26 @@ test('aborts eventually if server keeps returning 429', async () => {
 })
 
 test('aborts directly if server returns error other than 429', async () => {
-    const client = new SeatsioClient(new Region('https://mockbin.org'))
+    const client = new SeatsioClient(new Region('https://httpbin.org'))
     const start = new Date()
     try {
-        await client.client.get('/bin/1eea3aab-2bb2-4f92-99c2-50d942fb6294')
+        await client.client.get('/status/400')
         throw new Error('Should have failed')
     } catch (e) {
-        expect(e).toBe('get /bin/1eea3aab-2bb2-4f92-99c2-50d942fb6294 resulted in 400 Bad Request error')
+        expect(e).toBe('get /status/400 resulted in 400 BAD REQUEST error')
         const waitTime = new Date().getTime() - start.getTime()
         expect(waitTime).toBeLessThan(2000)
     }
 })
 
 test('aborts directly if server returns 429 but max retries 0', async () => {
-    const client = new SeatsioClient(new Region('https://mockbin.org')).setMaxRetries(0)
+    const client = new SeatsioClient(new Region('https://httpbin.org')).setMaxRetries(0)
     const start = new Date()
     try {
-        await client.client.get('/bin/0381d6f4-0155-4b8c-937b-73d3d88b2a3f')
+        await client.client.get('/status/429')
         throw new Error('Should have failed')
     } catch (e) {
-        expect(e).toEqual({ errors: [{ code: 'RATE_LIMIT_EXCEEDED', message: 'Rate limit exceeded' }], messages: [], requestId: '123456', status: 429 })
+        expect(e).toBe('get /status/429 resulted in 429 TOO MANY REQUESTS error')
         const waitTime = new Date().getTime() - start.getTime()
         expect(waitTime).toBeLessThan(2000)
     }
