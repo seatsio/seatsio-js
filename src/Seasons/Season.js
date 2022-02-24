be1550fe2eedd09ef721c2191de8a184213713ef
Matti Roloux <matti@seats.io>
2022-02-24 08:59:20 +0100

    Added additional season properties to events

--- src/Seasons/Season.js
+++ src/Seasons/Season.js
@@ -8,21 +8,10 @@ class Season extends Event {
         super(season)
         this.partialSeasonKeys = season.partialSeasonKeys
         this.events = season.events ? season.events.map(e => new Event(e)) : undefined
-        if (this.isTopLevelSeason()) {
-            this.seasonKey = undefined
-        }
     }
 
     isSeason () {
         return true
-    }
-
-    isTopLevelSeason () {
-        return this.partialSeasonKeys !== undefined
-    }
-
-    isPartialSeason () {
-        return !this.isTopLevelSeason()
     }
 }
 
