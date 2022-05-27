4e53bfdea531799a97e211c3fc7948095361a3c1
Matti Roloux <matti@seats.io>
2022-05-27 15:46:01 +0200

    Status changes are processed asynchronously

--- tests/events/listStatusChangesForObjects.test.js
+++ tests/events/listStatusChangesForObjects.test.js
@@ -1,16 +1,20 @@
 const testUtils = require('../testUtils.js')
+const { StatusChangeRequest } = require('../../index')
 
 test('should list status changes for objects', async () => {
     const { client, user } = await testUtils.createTestUserAndClient()
     const chartKey = testUtils.getChartKey()
     await testUtils.createTestChart(chartKey, user.secretKey)
     const event = await client.events.create(chartKey)
-    await client.events.changeObjectStatus(event.key, 'A-1', 's1')
-    await client.events.changeObjectStatus(event.key, 'A-1', 's2')
-    await client.events.changeObjectStatus(event.key, 'A-2', 's4')
-    await client.events.changeObjectStatus(event.key, 'A-1', 's3')
-    const statuses = []
+    await client.events.changeObjectStatusInBatch([
+        new StatusChangeRequest(event.key, 'A-1', 's1'),
+        new StatusChangeRequest(event.key, 'A-1', 's2'),
+        new StatusChangeRequest(event.key, 'A-2', 's4'),
+        new StatusChangeRequest(event.key, 'A-1', 's3')
+    ])
+    await testUtils.statusChangesPresent(client, event.key)
 
+    const statuses = []
     for await (const statusChange of client.events.statusChangesForObject(event.key, 'A-1').all()) {
         statuses.push(statusChange.status)
     }
