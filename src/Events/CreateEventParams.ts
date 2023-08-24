54417d2ed36106834eb935e80291ff149c352fdc
Matti Roloux <matti@seats.io>
2023-08-24 13:48:10 +0200

    Revert "Revert "Channels can be passed in when creating an event""
    This reverts commit 0763606eeb3358f523ac1dee4ee0d1c9284f3d15.

--- src/Events/CreateEventParams.ts
+++ src/Events/CreateEventParams.ts
@@ -1,4 +1,11 @@
 import { AbstractEventParams } from './AbstractEventParams'
+import { Channel } from './Channel'
 
 export class CreateEventParams extends AbstractEventParams {
+    channels?: Channel[]
+
+    withChannels (channels: Channel[]) {
+        this.channels = channels
+        return this
+    }
 }
