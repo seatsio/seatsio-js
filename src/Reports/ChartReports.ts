4b6d06891dc687b06a0e2ef142867f169bedd2e3
Steve Chaloner <schaloner@users.noreply.github.com>
2023-09-11 16:10:50 +0200

    Support draft reports for charts (#255)
    * Support draft reports for charts
    
    * Replaced almost-duplicated tests with parameterized tests
    
    * Corrected imports for eslint
    
    * Corrected imports for eslint on summary tests
    
    ---------
    
    Co-authored-by: Steve Chaloner <steve@seats.io>

--- src/Reports/ChartReports.ts
+++ src/Reports/ChartReports.ts
@@ -1,55 +1,60 @@
 import { Utilities } from '../utilities/reportUtility'
 import { Axios } from 'axios'
 
+export class Versions {
+    public static publishedVersion: string = 'published'
+    public static draftVersion: string = 'draft'
+}
+
 export class ChartReports {
     client: Axios
     constructor (client: Axios) {
         this.client = client
     }
 
-    byLabel (chartKey: string, bookWholeTables?: string) {
-        return this.fetchReport('byLabel', chartKey, bookWholeTables)
+    byLabel (chartKey: string, bookWholeTables?: string, version?: string) {
+        return this.fetchReport('byLabel', chartKey, bookWholeTables, version)
     }
 
-    byObjectType (chartKey: string, bookWholeTables?: string) {
-        return this.fetchReport('byObjectType', chartKey, bookWholeTables)
+    byObjectType (chartKey: string, bookWholeTables?: string, version?: string) {
+        return this.fetchReport('byObjectType', chartKey, bookWholeTables, version)
     }
 
-    summaryByObjectType (chartKey: string, bookWholeTables?: string) {
-        return this.fetchSummaryReport('byObjectType', chartKey, bookWholeTables)
+    summaryByObjectType (chartKey: string, bookWholeTables?: string, version?: string) {
+        return this.fetchSummaryReport('byObjectType', chartKey, bookWholeTables, version)
     }
 
-    byCategoryLabel (chartKey: string, bookWholeTables?: string) {
-        return this.fetchReport('byCategoryLabel', chartKey, bookWholeTables)
+    byCategoryLabel (chartKey: string, bookWholeTables?: string, version?: string) {
+        return this.fetchReport('byCategoryLabel', chartKey, bookWholeTables, version)
     }
 
-    summaryByCategoryLabel (chartKey: string, bookWholeTables?: string) {
-        return this.fetchSummaryReport('byCategoryLabel', chartKey, bookWholeTables)
+    summaryByCategoryLabel (chartKey: string, bookWholeTables?: string, version?: string) {
+        return this.fetchSummaryReport('byCategoryLabel', chartKey, bookWholeTables, version)
     }
 
-    byCategoryKey (chartKey: string, bookWholeTables?: string) {
-        return this.fetchReport('byCategoryKey', chartKey, bookWholeTables)
+    byCategoryKey (chartKey: string, bookWholeTables?: string, version?: string) {
+        return this.fetchReport('byCategoryKey', chartKey, bookWholeTables, version)
     }
 
-    summaryByCategoryKey (chartKey: string, bookWholeTables?: string) {
-        return this.fetchSummaryReport('byCategoryKey', chartKey, bookWholeTables)
+    summaryByCategoryKey (chartKey: string, bookWholeTables?: string, version?: string) {
+        return this.fetchSummaryReport('byCategoryKey', chartKey, bookWholeTables, version)
     }
 
-    bySection (chartKey: string, bookWholeTables?: string) {
-        return this.fetchReport('bySection', chartKey, bookWholeTables)
+    bySection (chartKey: string, bookWholeTables?: string, version?: string) {
+        return this.fetchReport('bySection', chartKey, bookWholeTables, version)
     }
 
-    summaryBySection (chartKey: string, bookWholeTables?: string) {
-        return this.fetchSummaryReport('bySection', chartKey, bookWholeTables)
+    summaryBySection (chartKey: string, bookWholeTables?: string, version?: string) {
+        return this.fetchSummaryReport('bySection', chartKey, bookWholeTables, version)
     }
 
-    fetchReport (reportType: string, chartKey: string, bookWholeTables?: string) {
-        return this.client.get(`/reports/charts/${encodeURIComponent(chartKey)}/${reportType}`, { params: { bookWholeTables } })
+    fetchReport (reportType: string, chartKey: string, bookWholeTables?: string, version?: string) {
+        return this.client.get(`/reports/charts/${encodeURIComponent(chartKey)}/${reportType}`, { params: { bookWholeTables, version } })
             .then(res => Utilities.createChartReport(res.data))
     }
 
-    fetchSummaryReport (reportType: string, chartKey: string, bookWholeTables?: string) {
-        return this.client.get(`/reports/charts/${encodeURIComponent(chartKey)}/${reportType}/summary`, { params: { bookWholeTables } })
+    fetchSummaryReport (reportType: string, chartKey: string, bookWholeTables?: string, version?: string) {
+        return this.client.get(`/reports/charts/${encodeURIComponent(chartKey)}/${reportType}/summary`, { params: { bookWholeTables, version } })
             .then(res => res.data)
     }
 }
