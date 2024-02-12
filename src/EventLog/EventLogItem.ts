4c3d314f54fa13ab941959a0b242e73285aa24d8
mroloux <matti@seats.io>
2024-02-12 13:37:59 +0100

    Tweaked event log items

--- src/EventLog/EventLogItem.ts
+++ src/EventLog/EventLogItem.ts
@@ -5,15 +5,13 @@ export type EventLogItemJson = Dict<any>
 export class EventLogItem {
     id: number
     type: string
-    workspaceKey: string
-    date: Date
+    timestamp: Date
     data?: Dict<any>
 
     constructor (json: EventLogItemJson) {
         this.id = json.id
         this.type = json.type
-        this.workspaceKey = json.workspaceKey
-        this.date = new Date(json.date)
+        this.timestamp = new Date(json.date)
         this.data = json.data
     }
 }
