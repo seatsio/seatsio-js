6eb956fa20f37560103b3d065c078ff18cd3568d
Ben Verbeken <ben@seats.io>
2025-07-01 09:49:01 +0200

    added companionSeatValidationEnabled field to DefaultRendererSettings

--- src/Accounts/DefaultRendererSettings.ts
+++ src/Accounts/DefaultRendererSettings.ts
@@ -5,9 +5,11 @@ type DefaultRendererSettingsJson = Dict<any>
 export class DefaultRendererSettings {
     multiSelectEnabled: boolean
     showFullScreenButton: boolean
+    companionSeatValidationEnabled: boolean
 
     constructor (json: DefaultRendererSettingsJson) {
         this.showFullScreenButton = json.showFullScreenButton
         this.multiSelectEnabled = json.multiSelectEnabled
+        this.companionSeatValidationEnabled = json.companionSeatValidationEnabled
     }
 }
