8162a73602d30af887cbd667220e13700bc2f8d0
mroloux <matti@seats.io>
2026-05-22 13:17:33 +0200

    Linting

--- tests/events/channels/addChannel.test.ts
+++ tests/events/channels/addChannel.test.ts
@@ -30,6 +30,20 @@ test('can add a channel', async () => {
     ])
 })
 
+test('channel has an id', async () => {
+    const { client, user } = await TestUtils.createTestUserAndClient()
+    const chartKey = TestUtils.getChartKey()
+    await TestUtils.createTestChart(chartKey, user.secretKey)
+    const event = await client.events.create(chartKey)
+
+    await client.events.channels.add(event.key, 'channelKey1', 'channel 1', '#FFFF98', 1, ['A-1'])
+
+    const retrievedEvent = await client.events.retrieve(event.key)
+    const channel = retrievedEvent.channels![0]
+    expect(channel.id).toBeTruthy()
+    expect(typeof channel.id).toBe('string')
+})
+
 test('can add multiple channels', async () => {
     const {
         client,
--- tests/events/channels/channelId.test.ts
+++ tests/events/channels/channelId.test.ts
@@ -1,19 +1,5 @@
 import { TestUtils } from '../../testUtils'
 
-test('channel has an id', async () => {
-    const { client, user } = await TestUtils.createTestUserAndClient()
-    const chartKey = TestUtils.getChartKey()
-    await TestUtils.createTestChart(chartKey, user.secretKey)
-    const event = await client.events.create(chartKey)
-
-    await client.events.channels.add(event.key, 'channelKey1', 'channel 1', '#FFFF98', 1, ['A-1'])
-
-    const retrievedEvent = await client.events.retrieve(event.key)
-    const channel = retrievedEvent.channels![0]
-    expect(channel.id).toBeTruthy()
-    expect(typeof channel.id).toBe('string')
-})
-
 test('areaPartitionLabel combines area label with channel id', async () => {
     const { client, user } = await TestUtils.createTestUserAndClient()
     const chartKey = TestUtils.getChartKey()
@@ -26,4 +12,3 @@ test('areaPartitionLabel combines area label with channel id', async () => {
     const channel = retrievedEvent.channels![0]
     expect(channel.areaPartitionLabel('GA1')).toBe(`GA1##${channel.id}`)
 })
-
