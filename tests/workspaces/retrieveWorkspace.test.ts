9c900b1c5cf7ad081679921760426e05b09e1e88
mroloux <matti@seats.io>
2026-07-08 14:21:29 +0200

    Unfix flaky test

--- tests/workspaces/retrieveWorkspace.test.ts
+++ tests/workspaces/retrieveWorkspace.test.ts
@@ -9,6 +9,7 @@ test('should retrieve a workspace', async () => {
     expect(retrievedWorkspace.key).toBe(workspace.key)
     expect(retrievedWorkspace.secretKey).toBe(workspace.secretKey)
     expect(retrievedWorkspace.id).toBe(workspace.id)
+    expect(retrievedWorkspace.settings).toEqual(workspace.settings)
     expect(retrievedWorkspace.name).toBe('a workspace')
     expect(retrievedWorkspace.isDefault).toBe(false)
     expect(retrievedWorkspace.isTest).toBe(false)
