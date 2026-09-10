7a8e756d009b3823a0dd20f50f7020c3974a305a
Steve Chaloner <schaloner@users.noreply.github.com>
2026-09-10 08:50:41 +0200

    Support reports for seasons and events in seasons where bookings are not propagated (#458)
    Co-authored-by: Steve Chaloner <steve@seats.io>

--- src/Reports/EventReports.ts
+++ src/Reports/EventReports.ts
@@ -5,163 +5,177 @@ import { EventObjectInfo } from '../Events/EventObjectInfo.js'
 
 export class EventReports {
     client: Axios
-    constructor (client: Axios) {
+    private readonly seasonBookingsPropagated: boolean
+
+    constructor (client: Axios, seasonBookingsPropagated: boolean = true) {
         this.client = client
+        this.seasonBookingsPropagated = seasonBookingsPropagated
+    }
+
+    withSeasonBookingsNotPropagated (): EventReports {
+        return new EventReports(this.client, false)
     }
 
     flatList (eventKey: string): Promise<EventObjectInfo[]> {
-        return this.client.get(`/reports/events/${encodeURIComponent(eventKey)}`)
+        return this.client.get(`/reports/events/${encodeURIComponent(eventKey)}`, { params: this.queryParams() })
             .then(res => (res.data as any[]).map(d => new EventObjectInfo(d)))
     }
 
     flatListCsv (eventKey: string): Promise<string> {
-        return this.client.get(`/reports/events/${encodeURIComponent(eventKey)}.csv`)
+        return this.client.get(`/reports/events/${encodeURIComponent(eventKey)}.csv`, { params: this.queryParams() })
             .then(res => res.data as string)
     }
 
     byStatus (eventKey: string, status: string | null = null) {
-        return this.client.get(EventReports.reportUrl('byStatus', eventKey, status))
+        return this.client.get(EventReports.reportUrl('byStatus', eventKey, status), { params: this.queryParams() })
             .then(res => Utilities.createEventReport(res.data))
     }
 
     summaryByStatus (eventKey: string) {
-        return this.client.get(EventReports.summaryReportUrl('byStatus', eventKey))
+        return this.client.get(EventReports.summaryReportUrl('byStatus', eventKey), { params: this.queryParams() })
             .then(res => res.data)
     }
 
     deepSummaryByStatus (eventKey: string) {
-        return this.client.get(EventReports.deepSummaryReportUrl('byStatus', eventKey))
+        return this.client.get(EventReports.deepSummaryReportUrl('byStatus', eventKey), { params: this.queryParams() })
             .then(res => res.data)
     }
 
     byObjectType (eventKey: string, objectType: string | null = null) {
-        return this.client.get(EventReports.reportUrl('byObjectType', eventKey, objectType))
+        return this.client.get(EventReports.reportUrl('byObjectType', eventKey, objectType), { params: this.queryParams() })
             .then(res => Utilities.createEventReport(res.data))
     }
 
     summaryByObjectType (eventKey: string) {
-        return this.client.get(EventReports.summaryReportUrl('byObjectType', eventKey))
+        return this.client.get(EventReports.summaryReportUrl('byObjectType', eventKey), { params: this.queryParams() })
             .then(res => res.data)
     }
 
     deepSummaryByObjectType (eventKey: string) {
-        return this.client.get(EventReports.deepSummaryReportUrl('byObjectType', eventKey))
+        return this.client.get(EventReports.deepSummaryReportUrl('byObjectType', eventKey), { params: this.queryParams() })
             .then(res => res.data)
     }
 
     byCategoryLabel (eventKey: string, categoryLabel: string | null = null) {
-        return this.client.get(EventReports.reportUrl('byCategoryLabel', eventKey, categoryLabel))
+        return this.client.get(EventReports.reportUrl('byCategoryLabel', eventKey, categoryLabel), { params: this.queryParams() })
             .then(res => Utilities.createEventReport(res.data))
     }
 
     summaryByCategoryLabel (eventKey: string) {
-        return this.client.get(EventReports.summaryReportUrl('byCategoryLabel', eventKey))
+        return this.client.get(EventReports.summaryReportUrl('byCategoryLabel', eventKey), { params: this.queryParams() })
             .then(res => res.data)
     }
 
     deepSummaryByCategoryLabel (eventKey: string) {
-        return this.client.get(EventReports.deepSummaryReportUrl('byCategoryLabel', eventKey))
+        return this.client.get(EventReports.deepSummaryReportUrl('byCategoryLabel', eventKey), { params: this.queryParams() })
             .then(res => res.data)
     }
 
     byCategoryKey (eventKey: string, categoryKey: CategoryKey | null = null) {
-        return this.client.get(EventReports.reportUrl('byCategoryKey', eventKey, categoryKey))
+        return this.client.get(EventReports.reportUrl('byCategoryKey', eventKey, categoryKey), { params: this.queryParams() })
             .then(res => Utilities.createEventReport(res.data))
     }
 
     summaryByCategoryKey (eventKey: string) {
-        return this.client.get(EventReports.summaryReportUrl('byCategoryKey', eventKey))
+        return this.client.get(EventReports.summaryReportUrl('byCategoryKey', eventKey), { params: this.queryParams() })
             .then(res => res.data)
     }
 
     deepSummaryByCategoryKey (eventKey: string) {
-        return this.client.get(EventReports.deepSummaryReportUrl('byCategoryKey', eventKey))
+        return this.client.get(EventReports.deepSummaryReportUrl('byCategoryKey', eventKey), { params: this.queryParams() })
             .then(res => res.data)
     }
 
     byLabel (eventKey: string, label: string | null = null) {
-        return this.client.get(EventReports.reportUrl('byLabel', eventKey, label))
+        return this.client.get(EventReports.reportUrl('byLabel', eventKey, label), { params: this.queryParams() })
             .then(res => Utilities.createEventReport(res.data))
     }
 
     byOrderId (eventKey: string, orderId: string | null = null) {
-        return this.client.get(EventReports.reportUrl('byOrderId', eventKey, orderId))
+        return this.client.get(EventReports.reportUrl('byOrderId', eventKey, orderId), { params: this.queryParams() })
             .then(res => Utilities.createEventReport(res.data))
     }
 
     bySection (eventKey: string, section: string | null = null) {
-        return this.client.get(EventReports.reportUrl('bySection', eventKey, section))
+        return this.client.get(EventReports.reportUrl('bySection', eventKey, section), { params: this.queryParams() })
             .then(res => Utilities.createEventReport(res.data))
     }
 
     summaryBySection (eventKey: string) {
-        return this.client.get(EventReports.summaryReportUrl('bySection', eventKey))
+        return this.client.get(EventReports.summaryReportUrl('bySection', eventKey), { params: this.queryParams() })
             .then(res => res.data)
     }
 
     deepSummaryBySection (eventKey: string) {
-        return this.client.get(EventReports.deepSummaryReportUrl('bySection', eventKey))
+        return this.client.get(EventReports.deepSummaryReportUrl('bySection', eventKey), { params: this.queryParams() })
             .then(res => res.data)
     }
 
     byZone (eventKey: string, zone: string | null = null) {
-        return this.client.get(EventReports.reportUrl('byZone', eventKey, zone))
+        return this.client.get(EventReports.reportUrl('byZone', eventKey, zone), { params: this.queryParams() })
             .then(res => Utilities.createEventReport(res.data))
     }
 
     summaryByZone (eventKey: string) {
-        return this.client.get(EventReports.summaryReportUrl('byZone', eventKey))
+        return this.client.get(EventReports.summaryReportUrl('byZone', eventKey), { params: this.queryParams() })
             .then(res => res.data)
     }
 
     deepSummaryByZone (eventKey: string) {
-        return this.client.get(EventReports.deepSummaryReportUrl('byZone', eventKey))
+        return this.client.get(EventReports.deepSummaryReportUrl('byZone', eventKey), { params: this.queryParams() })
             .then(res => res.data)
     }
 
     byAvailability (eventKey: string, availability: string | null = null) {
-        return this.client.get(EventReports.reportUrl('byAvailability', eventKey, availability))
+        return this.client.get(EventReports.reportUrl('byAvailability', eventKey, availability), { params: this.queryParams() })
             .then(res => Utilities.createEventReport(res.data))
     }
 
     byAvailabilityReason (eventKey: string, availabilityReason: string | null = null) {
-        return this.client.get(EventReports.reportUrl('byAvailabilityReason', eventKey, availabilityReason))
+        return this.client.get(EventReports.reportUrl('byAvailabilityReason', eventKey, availabilityReason), { params: this.queryParams() })
             .then(res => Utilities.createEventReport(res.data))
     }
 
     summaryByAvailability (eventKey: string) {
-        return this.client.get(EventReports.summaryReportUrl('byAvailability', eventKey))
+        return this.client.get(EventReports.summaryReportUrl('byAvailability', eventKey), { params: this.queryParams() })
             .then(res => res.data)
     }
 
     summaryByAvailabilityReason (eventKey: string) {
-        return this.client.get(EventReports.summaryReportUrl('byAvailabilityReason', eventKey))
+        return this.client.get(EventReports.summaryReportUrl('byAvailabilityReason', eventKey), { params: this.queryParams() })
             .then(res => res.data)
     }
 
     deepSummaryByAvailability (eventKey: string) {
-        return this.client.get(EventReports.deepSummaryReportUrl('byAvailability', eventKey))
+        return this.client.get(EventReports.deepSummaryReportUrl('byAvailability', eventKey), { params: this.queryParams() })
             .then(res => res.data)
     }
 
     deepSummaryByAvailabilityReason (eventKey: string) {
-        return this.client.get(EventReports.deepSummaryReportUrl('byAvailabilityReason', eventKey))
+        return this.client.get(EventReports.deepSummaryReportUrl('byAvailabilityReason', eventKey), { params: this.queryParams() })
             .then(res => res.data)
     }
 
     byChannel (eventKey: string, channel: string | null = null) {
-        return this.client.get(EventReports.reportUrl('byChannel', eventKey, channel))
+        return this.client.get(EventReports.reportUrl('byChannel', eventKey, channel), { params: this.queryParams() })
             .then(res => Utilities.createEventReport(res.data))
     }
 
     summaryByChannel (eventKey: string) {
-        return this.client.get(EventReports.summaryReportUrl('byChannel', eventKey))
+        return this.client.get(EventReports.summaryReportUrl('byChannel', eventKey), { params: this.queryParams() })
             .then(res => res.data)
     }
 
     deepSummaryByChannel (eventKey: string) {
-        return this.client.get(EventReports.deepSummaryReportUrl('byChannel', eventKey))
+        return this.client.get(EventReports.deepSummaryReportUrl('byChannel', eventKey), { params: this.queryParams() })
             .then(res => res.data)
+    }
+
+    private queryParams (): Record<string, boolean> {
+        if (this.seasonBookingsPropagated) {
+            return {}
+        }
+        return { seasonBookingsPropagated: false }
     }
 
     static reportUrl (reportType: string, eventKey: string, filter: string | number | null) {
--- tests/reports/events/eventReportFlatList.test.ts
+++ tests/reports/events/eventReportFlatList.test.ts
@@ -1,6 +1,7 @@
 import { TestUtils } from '../../testUtils.js'
 import { EventObjectInfo } from '../../../src/Events/EventObjectInfo.js'
 import { ObjectProperties } from '../../../src/Events/ObjectProperties.js'
+import { CreateSeasonParams } from '../../../src/Seasons/CreateSeasonParams.js'
 
 test('flatList returns flat list of EventObjectInfo sorted by label', async () => {
     const { client, user } = await TestUtils.createTestUserAndClient()
@@ -17,6 +18,24 @@ test('flatList returns flat list of EventObjectInfo sorted by label', async () =
     expect(report[0].status).toBe(EventObjectInfo.BOOKED)
     expect(report[1].label).toBe('A-2')
     expect(report[1].status).toBe(EventObjectInfo.FREE)
+})
+
+test('flatList with season bookings not propagated', async () => {
+    const { client, user } = await TestUtils.createTestUserAndClient()
+    const chartKey = TestUtils.getChartKey()
+    await TestUtils.createTestChart(chartKey, user.secretKey)
+    const season = await client.seasons.create(chartKey, new CreateSeasonParams().numberOfEvents(1))
+    const event = season.events![0]
+    await client.events.book(season.key, ['A-1', 'A-2'])
+    await client.events.book(event.key, ['A-3'])
+
+    const reportWithPropagation = await client.eventReports.flatList(season.key)
+    const reportWithoutPropagation = await client.eventReports.withSeasonBookingsNotPropagated().flatList(season.key)
+
+    const findByLabel = (report: EventObjectInfo[], label: string) => report.find(item => item.label === label)
+
+    expect(findByLabel(reportWithPropagation, 'A-3')!.status).toBe(EventObjectInfo.BOOKED)
+    expect(findByLabel(reportWithoutPropagation, 'A-3')!.status).not.toBe(EventObjectInfo.BOOKED)
 })
 
 test('flatListCsv returns a plain string', async () => {
