ea55a01edaf24ccfdd37bc6af8b407021f9f1daf
mroloux <matti@seats.io>
2026-09-21 09:21:48 +0200

    Use more parallel runners (#466)

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     test: {
         globals: true,
         testTimeout: 60000,
+        maxWorkers: 8,
         projects: [
             {
                 extends: true,
